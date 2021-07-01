2cd670bbacd41b21503497f9ee8a6490949ef381
Champions017 <81851859+Champions017@users.noreply.github.com>
2021-07-01 14:26:38 +0530

    Update introLevel1.js

--- introLevel1.js
+++ introLevel1.js
@@ -5,4 +5,7 @@ function intro1(){
     textFont("Comic Sans ms")
     textSize(35)
     text("LEVEL 1:\n\n\n\n\nIn this level, you need to move the dustbin to catch the bananas. \nDont let the bananas fall down!\nMove the dustbin with the arrow keys!\n\nPress 'space' to start!",width/2,height/2-200)
+    fill("red")
+    textSize(20)
+    text("𝑷𝒍𝒆𝒂𝒔𝒆 𝒏𝒐𝒕𝒆: 𝑻𝒉𝒊𝒔 𝒈𝒂𝒎𝒆 𝒄𝒂𝒏 𝒐𝒏𝒍𝒚 𝒃𝒆 𝒑𝒍𝒂𝒚𝒆𝒅 𝒐𝒏 𝒍𝒂𝒑𝒕𝒐𝒑𝒔 𝒕𝒉𝒂𝒕 𝒉𝒂𝒗𝒆 𝒂 14-𝒊𝒏𝒄𝒉 𝒍𝒂𝒓𝒈𝒆 𝒔𝒄𝒓𝒆𝒆𝒏, \n𝒏𝒐𝒕 𝒐𝒏 𝒐𝒏𝒆𝒔 𝒕𝒉𝒂𝒕 𝒉𝒂𝒗𝒆 𝒂 16 𝒊𝒏𝒄𝒉 𝒃𝒊𝒈 𝒐𝒏𝒆.",width/2,height-50)
 }
